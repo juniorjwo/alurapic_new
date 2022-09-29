617630befd032250447c83e434e646e8075c675c
juniorjwo <junioroliveira.fab@gmail.com>
2022-09-29 01:34:54 -0300

    implementando lazy loading.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,22 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { AppComponent } from './app.component';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { AppComponent } from "./app.component";
 
-import { PhotosModule } from './photos/photos.module';
-import { AppRoutingModule } from './app.routing.module';
-import { ErrorsModule } from './errors/errors.module';
-import { HomeModule } from './home/home.module';
-import { CoreModule } from './core/core.module';
+import { AppRoutingModule } from "./app.routing.module";
+import { CoreModule } from "./core/core.module";
+import { ErrorsModule } from "./errors/errors.module";
+import { PhotosModule } from "./photos/photos.module";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     PhotosModule,
-    AppRoutingModule,
     ErrorsModule,
-    HomeModule,
-    CoreModule
+    CoreModule,
+    AppRoutingModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -1,40 +1,23 @@
 import { NgModule } from "@angular/core";
-import { Routes, RouterModule } from "@angular/router";
+import { RouterModule, Routes } from "@angular/router";
 
-import { PhotoListComponent } from "./photos/photo-list/photo-list.component";
-import { PhotoFormComponent } from "./photos/photo-form/photo-form.component";
+
 import { NotFoundComponent } from "./errors/not-found/not-found.component";
+
+import { PhotoFormComponent } from "./photos/photo-form/photo-form.component";
+import { PhotoListComponent } from "./photos/photo-list/photo-list.component";
 import { PhotoListResolver } from "./photos/photo-list/photo-list.resolver";
-import { SignInComponent } from "./home/signin/signin.component";
-import { AuthGuard } from "./core/auth/auth.guard";
-import { SignUpComponent } from "./home/singup/singup.component";
-import { HomeComponent } from "./home/home.component";
 
 const routes: Routes = [
   {
     path: "",
-    component: HomeComponent,
-    canActivate: [AuthGuard],
-    children: [
-      {
-        path: "",
-        component: SignInComponent,
-        canActivate: [AuthGuard],
-      },
-      {
-        path: "signup",
-        component: SignUpComponent,
-      },
-    ],
+    pathMatch: "full",
+    redirectTo: "home",
   },
   {
-    path: "",
-    component: SignInComponent,
-    canActivate: [AuthGuard],
-  },
-  {
-    path: "signup",
-    component: SignUpComponent,
+    path: "home",
+    loadChildren:'./home/home.module#HomeModule'
+
   },
   {
     path: "user/:userName",
@@ -54,7 +37,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes,{useHash:true})],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
