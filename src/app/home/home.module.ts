b6786231152db99b8f4f6a1f27842cb0d8fc449b
juniorjwo <junioroliveira.fab@gmail.com>
2022-09-29 01:12:58 -0300

    Projeto novo

---
+++ src/app/home/home.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { SignInComponent } from './signin/signin.component';
+import { ReactiveFormsModule, FormsModule }  from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { VMessageModule } from '../shared/components/vmessage/vmessage.module';
+import { RouterModule } from '@angular/router';
+import { SignUpComponent } from './singup/singup.component';
+import { HomeComponent } from './home.component';
+
+@NgModule({
+    declarations: [
+        SignInComponent,
+        SignUpComponent ,
+        HomeComponent
+    ],
+    imports: [
+        CommonModule,
+        FormsModule,
+        ReactiveFormsModule,
+        VMessageModule,
+        RouterModule
+    ]
+})
+export class HomeModule { }
